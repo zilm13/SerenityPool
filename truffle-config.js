d9dc37643a1cf143475b3197bc27e3c85b010775
Dmitrii Shmatko <leodex23@gmail.com>
2021-03-16 11:51:22 +0300

    Added configuration for local Geth contract deploy

--- truffle-config.js
+++ truffle-config.js
@@ -5,18 +5,25 @@ module.exports = {
     // see <http://truffleframework.com/docs/advanced/configuration>
     // for more details on how to specify configuration options!
     //
-    //networks: {
-    //  development: {
-    //    host: "127.0.0.1",
-    //    port: 7545,
-    //    network_id: "*"
-    //  },
-    //  test: {
-    //    host: "127.0.0.1",
-    //    port: 7545,
-    //    network_id: "*"
-    //  }
-    //}
+    networks: {
+        local: {
+            host: "127.0.0.1",
+            port: 8545,
+            network_id: "*",
+            from: "0x2dbe6a7c756851b5239a118c71e16c95e33ecda8",
+            gas: "3141592"
+        },
+        //  development: {
+        //    host: "127.0.0.1",
+        //    port: 7545,
+        //    network_id: "*"
+        //  },
+        //  test: {
+        //    host: "127.0.0.1",
+        //    port: 7545,
+        //    network_id: "*"
+        //  }
+    },
     //
     compilers: {
         solc: {
