b1715455bf8f96a31944f1935c7bb639942ac80e
Dmitrii Shmatko <leodex23@gmail.com>
2021-03-25 17:48:21 +0300

    Refactored bitLength calculation to omit logarithm

--- contracts/SystemContract.sol
+++ contracts/SystemContract.sol
@@ -30,7 +30,6 @@ contract SystemContract is ISystemContract {
         assembly {
             root := beaconblockroot(slot)
         }
-        emit Logger(root);
         return root;
     }
 
@@ -85,7 +84,6 @@ contract SystemContract is ISystemContract {
                 sha256(abi.encodePacked(bytes32(0), bytes32(0)))
             ));
         bytes32 node = sha256(abi.encodePacked(two_root, three_root));
-        emit Logger(node);
         return node;
     }
 
@@ -144,41 +142,65 @@ contract SystemContract is ISystemContract {
     }
 
     function _getGeneralizedIndexLength(uint64 _index) internal pure returns (uint64) {
-        return uint64(_log2Floor(_index));
+        uint res = _bitLength(_index);
+        if (res == 0) {
+            return 0;
+        } else {
+            return uint64(res - 1);
+        }
     }
 
-    // Method copy-pasted from https://ethereum.stackexchange.com/a/30168
-    function _log2Floor(uint x) internal pure returns (uint){
-        uint y;
-        assembly {
-            let arg := x
-            x := sub(x,1)
-            x := or(x, div(x, 0x02))
-            x := or(x, div(x, 0x04))
-            x := or(x, div(x, 0x10))
-            x := or(x, div(x, 0x100))
-            x := or(x, div(x, 0x10000))
-            x := or(x, div(x, 0x100000000))
-            x := or(x, div(x, 0x10000000000000000))
-            x := or(x, div(x, 0x100000000000000000000000000000000))
-            x := add(x, 1)
-            let m := mload(0x40)
-            mstore(m,           0xf8f9cbfae6cc78fbefe7cdc3a1793dfcf4f0e8bbd8cec470b6a28a7a5a3e1efd)
-            mstore(add(m,0x20), 0xf5ecf1b3e9debc68e1d9cfabc5997135bfb7a7a3938b7b606b5b4b3f2f1f0ffe)
-            mstore(add(m,0x40), 0xf6e4ed9ff2d6b458eadcdf97bd91692de2d4da8fd2d0ac50c6ae9a8272523616)
-            mstore(add(m,0x60), 0xc8c0b887b0a8a4489c948c7f847c6125746c645c544c444038302820181008ff)
-            mstore(add(m,0x80), 0xf7cae577eec2a03cf3bad76fb589591debb2dd67e0aa9834bea6925f6a4a2e0e)
-            mstore(add(m,0xa0), 0xe39ed557db96902cd38ed14fad815115c786af479b7e83247363534337271707)
-            mstore(add(m,0xc0), 0xc976c13bb96e881cb166a933a55e490d9d56952b8d4e801485467d2362422606)
-            mstore(add(m,0xe0), 0x753a6d1b65325d0c552a4d1345224105391a310b29122104190a110309020100)
-            mstore(0x40, add(m, 0x100))
-            let magic := 0x818283848586878898a8b8c8d8e8f929395969799a9b9d9e9faaeb6bedeeff
-            let shift := 0x100000000000000000000000000000000000000000000000000000000000000
-            let a := div(mul(x, magic), shift)
-            y := div(mload(add(m,sub(255,a))), shift)
-            y := add(y, mul(256, gt(arg, 0x8000000000000000000000000000000000000000000000000000000000000000)))
+    function _bitLength(uint256 num) internal pure returns(uint) {
+        if (num == 0) {
+            return 0;
+        } else {
+            uint res = 0;
+            uint256 remaining = num;
+            while (remaining != 0) {
+                uint32 minorPart = uint32(remaining & 0xFFFFFFFF);
+                remaining >>= 32;
+                if (remaining != 0) {
+                    res += 32;
+                } else {
+                    res += _bitLengthForUInt32(minorPart);
+                }
+            }
+
+            return res;
         }
-        return (y - 1);
+    }
+
+    function _bitLengthForUInt32(uint32 n) internal pure returns(uint) {
+        return 32 - _numberOfLeadingZeros(n);
+    }
+
+    function  _numberOfLeadingZeros(uint32 i) internal pure returns(uint) {
+        if (i == 0) {
+            return 0;
+        } else {
+            uint n = 31;
+            if (i >= 65536) {
+                n -= 16;
+                i >>= 16;
+            }
+
+            if (i >= 256) {
+                n -= 8;
+                i >>= 8;
+            }
+
+            if (i >= 16) {
+                n -= 4;
+                i >>= 4;
+            }
+
+            if (i >= 4) {
+                n -= 2;
+                i >>= 2;
+            }
+
+        return n - (i >> 1);
+        }
     }
 
     function _getGeneralizedIndexBit(uint64 _index, uint _position) internal pure returns (bool) {
